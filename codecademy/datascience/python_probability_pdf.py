7a0822ff66b7fa26ba76869fcf98a3832802c58f
CodenificienT <61930093+codenificient@users.noreply.github.com>
2024-12-23 19:20:19 +0000

    add poisson distribution

--- codecademy/datascience/python_probability_pdf.py
+++ codecademy/datascience/python_probability_pdf.py
@@ -3,3 +3,14 @@ import scipy.stats as stats
 prob = stats.norm.cdf(175,167.64,8)
 print(prob)
 
+
+import scipy.stats as stats
+
+## Checkpoint 1
+temp_prob_1 = stats.norm.cdf(25, 20, 3) - stats.norm.cdf(18, 20, 3)
+print(temp_prob_1)
+
+## Checkpoint 2
+temp_prob_2 = 1 - stats.norm.cdf(24, 20, 3) 
+print(temp_prob_2)
+
